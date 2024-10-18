69e03134cd780f77aa8771de025a0710f0fa7167
Andreu <andreu.s.g30@gmail.com>
2024-10-18 18:14:35 +0200

    add login

---
+++ views/auth/login/login.php
@@ -0,0 +1,42 @@
+<?php
+// Andreu Sánchez Guerrero
+session_start(); // Iniciar sesión
+
+require_once __DIR__ . '/../../../config/database/connection.php';
+require_once BASE_PATH . 'controllers/CustomSessionHandler.php'; 
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    require_once BASE_PATH . 'controllers/loginController.php';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>views/components/header/header.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"> 
+</head>
+
+<body class="caja">
+    <div class="login-container">
+        <!-- Capçalera -->
+        <?php include BASE_PATH . 'views/components/header/header.php'; ?>
+        <h2 class="login-title">Login</h2>
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="form-login">
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+    </div>
+</body>
+</html>
