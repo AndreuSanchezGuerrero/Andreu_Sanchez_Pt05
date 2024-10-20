e854f2f15b5fe6f23f0894752d388fe726658115
Andreu <andreu.s.g30@gmail.com>
2024-10-20 11:34:54 +0200

    Use layout.css as a  barril sheet

--- views/layout.php
+++ views/layout.php
@@ -6,13 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestió d'Articles</title>
     <link rel="stylesheet" href="views/layout.css">
-    <link rel="stylesheet" href="views/components/header/header.css">
-    <link rel="stylesheet" href="views/auth/login/login.css">
-    <link rel="stylesheet" href="views/components/form/from.css">
-    <link rel="stylesheet" href="views/components/pagination/pagination.css">
-    <link rel="stylesheet" href="views/components/books/books.css">
-    <link rel="stylesheet" href="views/components/alert/alert.css">
-    <link rel="stylesheet" href="views/components/footer/footer.css">
     <script defer src="views/components/alert/alert.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"> 
 </head>
